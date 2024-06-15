722e6a92860d81bfbb5c3c290033bd7b94fb94b8
Steffen Vogel <post@steffenvogel.de>
2024-06-15 15:19:28 +0200

    feat(nix): Add Flake
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ flake.nix
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2023-2024 Steffen Vogel <post@steffenvogel.de>
+# SPDX-License-Identifier: Apache-2.0
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              go
+              golangci-lint
+              reuse
+            ];
+
+            packages = with pkgs; [
+              rosenpass
+            ];
+        };
+
+        formatter = nixpkgs.nixfmt-rfc-style;
+      }
+    );
+}
